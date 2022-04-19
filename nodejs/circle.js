9cd966d54d49799b73acb236643ed19e6f1f55b2
Afşin Loğoğlu <muhammedafsin@hotmail.com>
2022-04-19 09:46:09 +0300

    module dev
    circle.js den export edilen fonksiyonların index.js dosyasında kullanılması

---
+++ nodejs/circle.js
@@ -0,0 +1,15 @@
+
+function circleArea(r){
+    let area = Math.PI*r*r
+     console.log(area)
+}
+
+function circleCircumference(r){
+    let circumference = Math.PI*2*r
+     console.log(circumference)
+}
+
+module.exports = {
+    circleArea,
+    circleCircumference
+}
